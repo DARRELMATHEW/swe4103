81d33851f95102e125691401e64a29564f38e982
DARRELMATHEW <darrelmathew@gmail.com>
2025-10-21 00:01:23 -0300

    updated signup/login frontend UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,21 @@
 import React from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import Signup from "./components/signup";
-import Signin from "./components/signin";
-import Landing from "./components/landing"
-import Stusignin from "./components/stusignin";
+import Stusignup from "./components/Stusignup/Stusignup";
+import Signup from "./components/Signup/Signup"
+import Login from "./components/Login/Login";
+import Instructorsignup from "./components/Instructorsignup/Instructorsignup";
+import Claim from "./components/Claim/Claim";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Navigate to="/Home" />} />
-        <Route path="/Home" element={<Landing />} />
-        <Route path="/Instructor/SignUp" element={<Signup />} />
-        <Route path="/Instructor/SignIn" element={<Signin />} />
-        <Route path="/Student/SignIn" element={<Stusignin />} />
+        <Route path="/" element={<Navigate to="/login" />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/student-signup" element={<Stusignup />} />
+        <Route path="/instructor-signup" element={<Instructorsignup />} />
+        <Route path="/first-time-login" element={<Claim />} />
         <Route path="*" element={<h1>404 - Page Not Found</h1>} />
       </Routes>
     </BrowserRouter>
